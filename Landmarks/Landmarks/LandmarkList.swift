863035f30e57196b96138e518f91c89454b6633d
Misaki Shimazaki <msk.toybox@gmail.com>
2020-06-21 17:57:45 +0900

    [add] Section 5 Adopt the Model Object in Your Views

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -1,24 +1,17 @@
-/*
- See LICENSE folder for this sample’s licensing information.
- 
- Abstract:
- A view showing a list of landmarks.
- */
-
 import SwiftUI
 
 struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
-    
+
     var body: some View {
         NavigationView {
             List {
                 Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
-                
+
                 ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite  {
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
                         NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                             LandmarkRow(landmark: landmark)
                         }
@@ -32,11 +25,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-                .environmentObject(UserData())
-        }
+        LandmarkList()
+            .environmentObject(UserData())
     }
 }
